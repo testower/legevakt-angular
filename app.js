b629f0f49d381a5de25bfb100961f64f4f414827
Tom Erik Støwer <testower@gmail.com>
2013-10-26 14:57:26 +0200

    bootstrap

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-function AppController($scope) {
-	Parse.initialize("4DECQGbsZc1JkVLi02vuTlRdCaqmB49RC4EDaSIV", "kj3nbsej35OJXCIX1AlV1ILZH8rx8DIqLa1W6g4y");
+/*jslint node: true, nomen: true, vars: true */
+/*global Parse, angular, alert */
 
-	var HealthService = Parse.Object.extend("HealthService");
+'use strict';
 
-	function getHealthServices() {
-		var query = new Parse.Query(HealthService);
-		query.find({
-			success: function (results) {
-				$scope.$apply(function() {
-					$scope.healthServices = results.map(function(obj) {
-						return { displayName: obj.get("HealthServiceDisplayName"), parseObject: obj };
-					});
-				});
-			},
-			error: function (error) {
-				alert('Error: ' + error.code + " " + error.message);
-			}
-		});
-	}
+var myApp = angular.module('myApp', ['ui.bootstrap']);
 
-	getHealthServices();
-}
+myApp.controller('AppController', function AppController($scope) {
+    Parse.initialize("4DECQGbsZc1JkVLi02vuTlRdCaqmB49RC4EDaSIV", "kj3nbsej35OJXCIX1AlV1ILZH8rx8DIqLa1W6g4y");
+
+    var HealthService = Parse.Object.extend("HealthService");
+
+    function getHealthServices() {
+        var query = new Parse.Query(HealthService);
+        query.find({
+            success: function (results) {
+                $scope.$apply(function () {
+                    $scope.healthServices = results.map(function (obj) {
+                        return {
+                            displayName: obj.get("HealthServiceDisplayName"),
+                            parseObject: obj,
+                            address: obj.get("VisitAddressStreet")
+                                + " "
+                                + obj.get("VisitAddressPostName"),
+                            shouldHideDetails: true
+                        };
+                    });
+                });
+            },
+            error: function (error) {
+                alert('Error: ' + error.code + " " + error.message);
+            }
+        });
+    }
+
+    getHealthServices();
+
+    $scope.showDetails = function (healthService) {
+        healthService.shouldHideDetails = !healthService.shouldHideDetails;
+    };
+});
